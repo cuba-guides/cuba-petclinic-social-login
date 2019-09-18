aa2b74f6247741e94ce5b7a9277282db726177b8
Daniil Tsarev <tsarev@haulmont.com>
2019-09-18 16:29:38 +0400

    Update preview images

--- modules/web/src/com/haulmont/sample/petclinic/web/ExtMainScreen.java
+++ modules/web/src/com/haulmont/sample/petclinic/web/ExtMainScreen.java
@@ -4,10 +4,8 @@ import com.haulmont.cuba.gui.Screens;
 import com.haulmont.cuba.gui.components.mainwindow.UserActionsButton;
 import com.haulmont.cuba.gui.screen.Install;
 import com.haulmont.cuba.gui.screen.OpenMode;
-import com.haulmont.cuba.gui.screen.Subscribe;
 import com.haulmont.cuba.gui.screen.UiController;
 import com.haulmont.cuba.gui.screen.UiDescriptor;
-import com.haulmont.cuba.web.Connection;
 import com.haulmont.cuba.web.app.main.MainScreen;
 
 import javax.inject.Inject;
@@ -18,19 +16,11 @@ public class ExtMainScreen extends MainScreen {
 
     @Inject
     private Screens screens;
-    @Inject
-    private Connection connection;
 
+    @SuppressWarnings("unused")
     @Install(to = "userActionsButton", subject = "loginHandler")
     private void loginHandler(UserActionsButton.LoginHandlerContext ctx) {
         showLoginDialog();
-    }
-
-    @Subscribe
-    private void onAfterShowExt(AfterShowEvent event) {
-        if (!connection.isAuthenticated()) {
-            showLoginDialog();
-        }
     }
 
     private void showLoginDialog() {
